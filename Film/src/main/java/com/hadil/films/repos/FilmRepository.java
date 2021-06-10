96a33b8bf84d62ca4ddc3071202704a5762d8110
HadilAbdennebi <77753409+HadilAbdennebi@users.noreply.github.com>
2021-06-10 18:09:56 +0200

    Add files via upload

---
+++ Film/src/main/java/com/hadil/films/repos/FilmRepository.java
@@ -0,0 +1,39 @@
+package com.hadil.films.repos;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.hadil.films.entities.Film;
+import com.hadil.films.entities.Genre;
+
+public interface FilmRepository extends JpaRepository<Film, Long> {
+		List<Film> findByNomFilm(String nom);
+		 List<Film> findByNomFilmContains(String nom);
+		 
+
+		 @Query("select p from Film p where p.nomFilm like %:nom and p.prixTicket > :prix")
+		 List<Film> findByNomPrix (@Param("nom") String nom,@Param("prix") Double prix);
+		 @Query("SELECT f FROM Film f WHERE f.nomFilm LIKE %?1%"
+					+ "Or f.genre like %?1%")
+		    public List<Film> findAll(String keyword);
+		 //List<Film> findByGenre (String keyword);
+		// List<Film> findByGenre (String genre);
+
+		 List<Film> findByGenreNomGenre(String keyword);
+
+		// @Query("select p from Film p where p.genre is like %?1%")
+		 //List<Film> findByGenre (Genre genre);
+		// List<Film> findByGenreIdGen(Long id);
+		 List<Film> findByOrderByNomFilmAsc();
+		 
+		 @Query("select p from Film p order by p.nomFilm ASC, p.prixTicket DESC")
+		 List<Film> trierFilmsNomsPrix ();
+		List<Film> findByGenre(String genre);
+		 
+
+
+
+}
